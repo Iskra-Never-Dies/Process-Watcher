83c01a9def936a8f2720f1644b9c0cb55783ee9d
Iskra-Never-Dies <73405388+Iskra-Never-Dies@users.noreply.github.com>
2022-06-13 15:43:31 +0200

    Add files via upload

---
+++ ProcessWatcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace KatoAPI
+{
+    internal class ProcessWatcher
+    {
+        internal Process Process = new Process();
+
+        internal delegate void Delegate(Process process);
+        internal event Delegate Created;
+
+        internal bool GetProcess()
+        {
+            Process[] processes = Process.GetProcessesByName("RobloxPlayerBeta"); //process name
+            if (processes.Length < 1)
+                return false;
+
+            //this shit is slightly autistic, 2lazy to fix
+            foreach (var process in processes)
+            {
+                if (String.IsNullOrWhiteSpace(process.MainWindowTitle))
+                    continue;
+
+                Process = process;
+                return true;
+            }
+
+            return false;
+        }
+
+        internal void Auto()
+        {
+            do
+                Thread.Sleep(2000); //lol
+            while (!GetProcess());
+
+            Created?.Invoke(Process);
+
+            Process.EnableRaisingEvents = true;
+            Process.Exited += (ignore, ignore2) =>
+                Auto();
+        }
+    }
+}
